ea321d77044ad6c447ca30282a5ea00110836596
Engramar Bollas <82358440+ebollas@users.noreply.github.com>
2021-05-03 07:17:44 +1000

    initial version

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var jsonServer  = require('json-server')
-var server      = jsonServer.create()
-var router      = jsonServer.router(require('./db.js')())
-var middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, function () {
-console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
